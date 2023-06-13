98fd672f43de8c440f9c9590734277a2b67ab017
Eryan GAUVRIT <eryan.gauvrit@gmail.com>
2023-06-13 20:11:39 +0200

    title home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,8 +2,15 @@
 
 function Home() {
     return (
-        <main>
-            <h1>A l'Est de la Grosne</h1>
+        <main className="main-home">
+            <div className="title-home">
+                <div>
+                    <h1>À l'Est de la Grosne</h1>
+                    <h2>Gîtes de Bourgogne</h2>
+                </div>
+                <h3>Idéalement situés entre la côte chalonnaise et la côte mâconnaise</h3>
+            </div>
+
         </main>
     )
 };
