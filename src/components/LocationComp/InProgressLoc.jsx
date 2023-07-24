b1a73cbb03e7ffc47959d33b6efba4bdd6fbbdfa
Eryan GAUVRIT <eryan.gauvrit@gmail.com>
2023-07-24 13:11:22 +0200

    fix inProgressLoc style

--- src/components/LocationComp/InProgressLoc.jsx
+++ src/components/LocationComp/InProgressLoc.jsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
 import ButtonLink from "../ButtonLink";
+import { choosePicture } from "../utils/ChoosePicture";
 
 
 function InProgressLoc({ locationsDatas }) {
+
+    const iconList = <i className="fa-regular fa-hand-pointer"></i>;
+
     return (
         <article className="inProgressLoc">
             <p>Nous vous invitons à consulter nos gîtes actuellement ouverts :</p>
             <ul>
                 {
                     locationsDatas.map((location) =>
-                        !location.inProgress && <li key={location.id}><Link to={`/location/${location.id}`} onClick={"#backTop-anchor"}>{location.title}</Link></li>
+                        !location.inProgress && <li key={location.id}><Link to={`/location/${location.id}`} onClick={"#backTop-anchor"}>
+                            {iconList}
+                            <img src={choosePicture(location.logoDesktop, location.logoPhone)} alt={location.title} />
+                        </Link></li>
                     )
                 }
             </ul>
