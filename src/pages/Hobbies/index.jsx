5194419a2a7960b6953fc44d6c6325e345b38aa5
Eryan GAUVRIT <eryan.gauvrit@gmail.com>
2023-10-11 20:42:29 +0200

    hobbies cards position

--- src/pages/Hobbies/index.jsx
+++ src/pages/Hobbies/index.jsx
@@ -31,57 +31,69 @@ function Hobbies({ events }) {
                 <article>
                     <h3>Activités sportives</h3>
                     <div className="hobbies-bloc">
-                        <HobbiesCard
-                            title={'Escalade'}
-                            description={"2 sites à proximité :"}
-                            listLink={[
-                                { link: 'http://escalade-cote-sud.fr/2020/09/29/etrigny/', content: "À Etrigny - 5 kms" },
-                                { link: 'http://escalade-cote-sud.fr/2020/06/12/culles-les-roches-topo-ffme/', content: "Culles les Roches - 10 kms" }
-                            ]}
-                            pictures={[climb(1), climb(2)]}
-                        />
-                        <HobbiesCard
-                            title={'Randonnées à vélo'}
-                            description={`A 5 kms du gîte vous trouverez la voie verte.
+                        <div className="hobbies-card-placement">
+                            <HobbiesCard
+                                title={'Escalade'}
+                                description={"2 sites à proximité :"}
+                                listLink={[
+                                    { link: 'http://escalade-cote-sud.fr/2020/09/29/etrigny/', content: "À Etrigny - 5 kms" },
+                                    { link: 'http://escalade-cote-sud.fr/2020/06/12/culles-les-roches-topo-ffme/', content: "Culles les Roches - 10 kms" }
+                                ]}
+                                pictures={[climb(1), climb(2)]}
+                            />
+                        </div>
+                        <div className="hobbies-card-placement">
+                            <HobbiesCard
+                                title={'Randonnées à vélo'}
+                                description={`A 5 kms du gîte vous trouverez la voie verte.
                             Il est possible d’effectuer une grande boucle Cluny – Mâcon – Tournus – Chalon-sur-Saône – Cluny,
                             sur 145 km de balades et découvertes.`}
-                            listText={["Nous pouvons vous mettre des vélos et tandems à disposition (sur demande à la réservation du gîte)."]}
-                            iconListText={false}
-                            listLink={[
-                                { link: 'https://www.destination-saone-et-loire.fr/fr/nos-destinations/a-velo.html', content: "destination-saone-et-loire.fr" },
-                                { link: 'https://www.francevelotourisme.com/itineraire/bourgogne-du-sud-chalon-sur-saone-macon', content: "francevelotourisme.com" }
-                            ]}
-                            btnLink={false}
-                            btnText={false}
-                            pictures={[bike()]}
-                        />
-                        <HobbiesCard
-                            title={'Randonnées'}
-                            description={`A 30m du gîte, vous trouverez des départs de sentiers et la Forêt du Grand Bragny.
+                                listText={["Nous pouvons vous mettre des vélos et tandems à disposition (sur demande à la réservation du gîte)."]}
+                                iconListText={false}
+                                listLink={[
+                                    { link: 'https://www.destination-saone-et-loire.fr/fr/nos-destinations/a-velo.html', content: "destination-saone-et-loire.fr" },
+                                    { link: 'https://www.francevelotourisme.com/itineraire/bourgogne-du-sud-chalon-sur-saone-macon', content: "francevelotourisme.com" }
+                                ]}
+                                btnLink={false}
+                                btnText={false}
+                                pictures={[bike()]}
+                            />
+                        </div>
+                        <div className="hobbies-card-placement">
+                            <HobbiesCard
+                                title={'Randonnées'}
+                                description={`A 30m du gîte, vous trouverez des départs de sentiers et la Forêt du Grand Bragny.
                             Vous trouverez également d’innombrables itinéraires de randonnées pour tous les niveaux :
                             La Roche de Solutré, le chemin des Moines, balades dans les vignobles ….`}
-                            iconListLink={false}
-                            btnLink={"https://www.destination-saone-et-loire.fr/fr/nos-pepites/les-balades-et-randos.html"}
-                            btnText={"En savoir plus ?"}
-                            pictures={[hiking(1), hiking(2)]}
-                        />
-                        <div className="hobbies-bloc-div ">
+                                iconListLink={false}
+                                btnLink={"https://www.destination-saone-et-loire.fr/fr/nos-pepites/les-balades-et-randos.html"}
+                                btnText={"En savoir plus ?"}
+                                pictures={[hiking(1), hiking(2)]}
+                            />
+                        </div>
+                        <div className="hobbies-card-placement">
                             <HobbiesCard
                                 title="Equitation"
                                 description="Plusieurs centres à proximité du gîte (situés entre 1 et 10 kms) :"
                                 listText={["La Chapelle de Bragny", "Corlay", "Messey sur Grosne", "Etrigny"]}
                                 iconListLink={false}
                             />
+                        </div>
+                        <div className="hobbies-card-placement">
                             <HobbiesCard
                                 title="Terrains de tennis"
                                 description="2 sites à proximité :"
                                 listText={["Etrigny – 5 kms", "Sennecey le Grand – 10 kms"]}
                                 iconListLink={false}
                             />
+                        </div>
+                        <div className="hobbies-card-placement">
                             <HobbiesCard
                                 title="Piscines et lieux de baignade les plus proches :"
                                 listLink={[{ link: "https://www.senneceylegrand.fr/piscine-municipale", content: "Piscine découverte à Sennecey le Grand – 10 kms" }, { link: "https://www.bourgogne-tourisme.com/baignade/base-nautique-lacs-de-laives", content: "Base nautique des lacs de Laives – 10 kms" }]}
                             />
+                        </div>
+                        <div className="hobbies-card-placement">
                             <HobbiesCard
                                 title="Pêche (dans la Grosne, la Saône)"
                                 description="La rivière la Grosne est à 200m du gîte !"
@@ -89,6 +101,8 @@ function Hobbies({ events }) {
                                 iconListText={false}
                                 listLink={[{ link: "https://www.peche-saone-et-loire.fr/ou-pecher/les-rivieres/grosne/", content: "En savoir plus ?" }]}
                             />
+                        </div>
+                        <div className="hobbies-card-placement">
                             <HobbiesCard
                                 title="Voies navigables"
                                 pictures={[waterway()]}
@@ -101,52 +115,45 @@ function Hobbies({ events }) {
                 </article>
                 <article>
                     <h3>Patrimoine</h3>
-                    <ul className="hobbies-bloc">
-                        <li className="hobbies-bloc-list">
-                            <h4>Cluny / Dijon / Beaune / Chalon sur Saône / Macon / Tournus / Chapaize ….</h4>
-                            <SlideShow
+                    <div className="hobbies-bloc">
+                        <div className="hobbies-card-placement">
+                            <HobbiesCard
+                                title="Cluny / Dijon / Beaune / Chalon sur Saône / Macon / Tournus / Chapaize …."
                                 pictures={heritage()}
-                                onClick={false}
+                                btnLink="https://www.destination-saone-et-loire.fr/fr/71-experiences/patrimoine-et-artisanat-dart.html"
+                                btnText="Toutes les informations"
+                                iconListLink={false}
                             />
-                            <ButtonLink
-                                link={"https://www.destination-saone-et-loire.fr/fr/71-experiences/patrimoine-et-artisanat-dart.html"}
-                                content={"Toutes les informations"}
-                                externalNavigation={true}
-
+                        </div>
+                        <div className="hobbies-card-placement">
+                            <HobbiesCard
+                                title="Les grottes de Blanot / Les grottes d’Azé …."
+                                pictures={[cave()]}
+                                iconListLink={false}
                             />
-                        </li>
-                        <li className="hobbies-bloc-list">
-                            <h4>Les grottes de Blanot / Les grottes d’Azé ….</h4>
-                            <img src={cave()} alt="Les grottes de Blanot / Les grottes d’Azé" />
-                        </li>
-                        <li className="hobbies-bloc-list">
-                            <h4>Châteaux de Brancion / Cormatin ….</h4>
-                            <img src={castle()} alt="Châteaux de Brancion / Cormatin" />
-                        </li>
-                        <li className="hobbies-bloc-list">
-                            <h4>La route des vins</h4>
-                            <img src={wineRoad(2)} alt="La route des vins" />
-                        </li>
-                        <li className="hobbies-bloc-list">
-                            <h4>Gastronomie</h4>
-                            <ul>
-                                <li>{icon}Marchés</li>
-                                <li>{icon}Restaurants</li>
-                                <li>{icon}Caves</li>
-                                <li>{icon}...</li>
-                            </ul>
-                            <ButtonLink
-                                link={"https://www.vigneronsdebuxy.fr/"}
-                                content={"Les Vignerons de Buxy"}
-                                externalNavigation={true}
+                        </div>
+                        <div className="hobbies-card-placement">
+                            <HobbiesCard
+                                title="Châteaux de Brancion / Cormatin …."
+                                pictures={[castle()]}
+                                iconListLink={false}
                             />
-                            <ButtonLink
-                                link={"https://www.cave-bissey.com/"}
-                                content={"Cave de Bissey"}
-                                externalNavigation={true}
+                        </div>
+                        <div className="hobbies-card-placement">
+                            <HobbiesCard
+                                title="La route des vins"
+                                pictures={[wineRoad(2)]}
+                                iconListLink={false}
                             />
-                        </li>
-                    </ul>
+                        </div>
+                        <div className="hobbies-card-placement">
+                            <HobbiesCard
+                                title="Châteaux de Brancion / Cormatin …."
+                                listText={["Marchés", "Restaurants", "Caves", "..."]}
+                                listLink={[{ link: "https://www.vigneronsdebuxy.fr/", content: "Les Vignerons de Buxy" }, { link: "https://www.cave-bissey.com/", content: "Cave de Bissey" }]}
+                            />
+                        </div>
+                    </div>
                 </article>
             </section>
 
