e079c5804cd7845e39fdf7ee0188c3d31c8da828
Eryan GAUVRIT <eryan.gauvrit@gmail.com>
2023-10-13 17:57:28 +0200

    FIX - loading data on error page

--- src/components/ErrorComp/index.jsx
+++ src/components/ErrorComp/index.jsx
@@ -1,13 +1,32 @@
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import SpinnerLoader from "../SpinnerLoader";
 
 function ErrorComp() {
+
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 10000)
+    })
+
     return (
-        <main className="main-error">
-            <h1>ERREUR : 404</h1>
-            <h2>Dommage ! Cette page est introuvable ...</h2>
-            <Link to={'/'} onClick={"#backTop-anchor"}>Retour sur la page d'accueil</Link>
-        </main>
+        <React.Fragment>
+            {
+                loading ?
+                    <main className="main-error loading-page">
+                        <SpinnerLoader isLoad={loading} />
+                    </main>
+                    :
+                    <main className="main-error">
+                        <h1>ERREUR : 404</h1>
+                        <h2>Dommage ! Cette page est introuvable ...</h2>
+                        <Link to={'/'} onClick={"#backTop-anchor"}>Retour sur la page d'accueil</Link>
+                    </main>
+            }
+        </React.Fragment>
     )
 };
 
